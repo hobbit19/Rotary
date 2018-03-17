2247e419d0cf924a8dc396b644bb44b945d57dff
Robert Nash <robscode@icloud.com>
2018-03-17 12:46:13 +0000

    Install default implementation for wheel option title

--- Rotary/WheelOption.swift
+++ Rotary/WheelOption.swift
@@ -11,3 +11,10 @@ import Foundation
 public protocol WheelOption: Equatable {
     var wheelOptionTitle: String { get }
 }
+
+public extension WheelOption where Self: CustomStringConvertible {
+  
+  var wheelOptionTitle: String {
+    return description
+  }
+}
